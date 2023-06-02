e3da8d6b1fa1aa18c15b937a80314e2dcdad03b3
Jinsu Kim <akrn112@naver.com>
2023-06-02 19:24:47 +0900

    third commit

---
+++ src/main/java/Mainsecond.java
@@ -0,0 +1,321 @@
+import java.util.*;
+import java.util.List;
+
+public class Mainsecond {
+    public static void main(String[] args) {
+
+
+        List<Student> studentlist = new sample().studentList;
+        // Sample student data with subjects and
+        Map<Integer, List<String>> students = new HashMap<>();
+        for (Student student : studentlist)
+        {
+            List<String> subjecetlist = new ArrayList<>();
+            if(student.getSubject1() != null)
+                subjecetlist.add(student.getSubject1());
+            if(student.getSubject2() != null)
+                subjecetlist.add(student.getSubject2());
+            if(student.getSubject3() != null)
+                subjecetlist.add(student.getSubject3());
+            students.put(student.getStudentId(), subjecetlist); //if different professor and same subject -> try use concat
+
+        }
+
+
+//        if(new ArrayList(Arrays.asList("Math", "Physics", "Chemistry")).containsAll(new ArrayList(Arrays.asList("Math", "Physics", "Chemistry"))))
+//                System.out.println("true");
+        List<List<Integer>> newstudyGroups = new ArrayList<>();
+        List<List<Integer>> friendstudyGroups = new ArrayList<>();
+
+        Set<Integer> visitedStudents = new HashSet<>();
+
+
+        //first matching
+        for (Student outstudent : studentlist) {
+            if (visitedStudents.contains(outstudent.getStudentId()) || outstudent.getGroupId() == 0)
+                continue;
+            List<Integer> group = new ArrayList<>();
+
+            for (Student innerstudent : studentlist) {
+                if (outstudent.getStudentId() == innerstudent.getStudentId() || innerstudent.getGroupId() == 0 || visitedStudents.contains(innerstudent.getStudentId()))
+                    continue;
+                if (outstudent.getGroupId() == innerstudent.getGroupId()) {
+                    if (!visitedStudents.contains(outstudent.getStudentId())) {
+                        visitedStudents.add(outstudent.getStudentId());
+                        group.add(outstudent.getStudentId());
+                    }
+                    if (!visitedStudents.contains(innerstudent.getStudentId())) {
+                        visitedStudents.add(innerstudent.getStudentId());
+                        group.add(innerstudent.getStudentId());
+                    }
+                }
+            }
+            friendstudyGroups.add(group);
+        }
+        // Display the friend study groups
+        System.out.println("after frist matcning");
+        int groupNumber = 1;
+        for (List<Integer> group : friendstudyGroups) {
+            System.out.println("FriendGroup " + groupNumber + ": ");
+            for (int id : group)
+                System.out.println("studentId " + id + " : " + students.get(id));
+            groupNumber++;
+        }
+
+        // second matching
+        for (Map.Entry<Integer, List<String>> entry : students.entrySet()) {
+            int studentId = entry.getKey();
+            if (visitedStudents.contains(studentId)) {
+                continue; // Skip if the student is already assigned to a group
+            }
+
+            List<String> currentSubjects = entry.getValue();
+            List<Integer> group = new ArrayList<>();
+            group.add(studentId);
+            visitedStudents.add(studentId);
+
+
+            // Find other students with matching subjects
+            for (Map.Entry<Integer, List<String>> innerEntry : students.entrySet()) {
+                int innerStudentId = innerEntry.getKey();
+                if (studentId == innerStudentId || visitedStudents.contains(innerStudentId)) {
+                    continue; // Skip if the student is already assigned to a group or is the same student
+                }
+
+                List<String> innerSubjects = innerEntry.getValue();
+
+                if (match_multiplesubject(currentSubjects, innerSubjects)) {
+                    int length = 0;
+                    for (Integer member : group) { //compare studentid of current group memeber to innerstudentid
+                        if (member == studentId) // except studentId taking currentSubject
+                            continue;
+                        if (match_multiplesubject(students.get(member), innerSubjects))
+                            length++;
+                    }
+                    if (length == group.size() - 1) {//except studentId taking currentSubject
+                        visitedStudents.add(innerStudentId);
+                        group.add(innerStudentId);
+                    }
+
+                    if (group.size() > 5) {
+                        break; // Stop adding students once the group has at least 3 members
+                    }
+                }
+            }
+            if (group.size() == 1) {
+                visitedStudents.remove(studentId); //modified
+            }
+
+            // Add the group to the study groups list
+            if (group.size() >= 2) {
+                newstudyGroups.add(group);
+            }
+        }
+
+
+        //special matching -> comparing one subject
+        for (Map.Entry<Integer, List<String>> entry : students.entrySet()) {
+            int studentId = entry.getKey();
+            if (visitedStudents.contains(studentId)) {
+                continue; // Skip if the student is already assigned to a group
+            }
+
+            List<String> currentSubjects = entry.getValue();
+            List<Integer> group = new ArrayList<>();
+            group.add(studentId);
+            visitedStudents.add(studentId);
+
+
+            // Find other students with matching subjects
+            for (Map.Entry<Integer, List<String>> innerEntry : students.entrySet()) {
+                int innerStudentId = innerEntry.getKey();
+                if (studentId == innerStudentId || visitedStudents.contains(innerStudentId)) {
+                    continue; // Skip if the student is already assigned to a group or is the same student
+                }
+
+                List<String> innerSubjects = innerEntry.getValue();
+
+                if (match_onesubject(currentSubjects, innerSubjects)) {
+                    int length = 0;
+                    for (Integer member : group) { //compare studentid of current group memeber to innerstudentid
+                        if (member == studentId) // except studentId taking currentSubject
+                            continue;
+                        if (match_onesubject(students.get(member), innerSubjects))
+                            length++;
+                    }
+                    if (length == group.size() - 1) {//except studentId taking currentSubject
+                        visitedStudents.add(innerStudentId);
+                        group.add(innerStudentId);
+                    }
+
+                    if (group.size() > 5) {
+                        break; // Stop adding students once the group has at least 3 members
+                    }
+                }
+            }
+            if (group.size() == 1) {
+                visitedStudents.remove(studentId); //modified
+            }
+
+            // Add the group to the study groups list
+            if (group.size() >= 2) {
+                newstudyGroups.add(group);
+            }
+        }
+
+
+        // Display the study groups
+//        int groupNumber = 1;
+        System.out.println();
+        System.out.println("after second matching and special matching withtout friend group");
+        for (List<Integer> group : newstudyGroups) {
+            System.out.println("Group " + groupNumber + ": ");
+            for (int id : group)
+                System.out.println("studentId " + id + " : " + students.get(id));
+            groupNumber++;
+        }
+
+
+        //3rd matching newstudygroup
+        for (List<Integer> group : newstudyGroups) {
+            if (group.size() > 5)
+                continue;
+            int firstid = group.get(0);
+            List<String> commonsubjects = new ArrayList<>(students.get(firstid));
+            for (int studentId : group) {
+                if (studentId == firstid)
+                    continue;
+                List<String> comparesubjects = students.get(studentId);
+                commonsubjects.retainAll(comparesubjects); //remove not common element in commonsubjects and return true if commonsubjects changed as a result of the call
+                if (commonsubjects.size() == 0)
+                    break;
+            }
+
+            for (Map.Entry<Integer, List<String>> innerEntry : students.entrySet()) {
+                int innerStudentId = innerEntry.getKey();
+                if (visitedStudents.contains(innerStudentId)) {
+                    continue; // Skip if the student is already assigned to a group or is the same student
+                }
+                List<String> innerSubjects = innerEntry.getValue();
+
+                commonsubjects.retainAll(innerSubjects);  //remove not common element in commonsubjects and return true if commonsubjects changed as a result of the call
+                if (commonsubjects.size() > 0) {
+                    group.add(innerStudentId);
+                    visitedStudents.add(innerStudentId);
+                }
+                if (group.size() > 5) {
+                    break;
+                }
+
+
+            }
+
+        }
+        //3rd matcning friendstudygroup
+        for (List<Integer> group : friendstudyGroups) {
+            if (group.size() > 3)
+                continue;
+            int firstid = group.get(0);
+            List<String> commonsubjects = new ArrayList<>(students.get(firstid));
+            for (int studentId : group) {
+                if (studentId == firstid)
+                    continue;
+                List<String> comparesubjects = students.get(studentId);
+                commonsubjects.retainAll(comparesubjects); //remove not common element in commonsubjects and return true if commonsubjects changed as a result of the call
+                if (commonsubjects.size() == 0)
+                    break;
+
+            }
+
+            for (Map.Entry<Integer, List<String>> innerEntry : students.entrySet()) {
+                int innerStudentId = innerEntry.getKey();
+                if (visitedStudents.contains(innerStudentId)) {
+                    continue; // Skip if the student is already assigned to a group or is the same student
+                }
+                List<String> innerSubjects = innerEntry.getValue();
+
+                commonsubjects.retainAll(innerSubjects);  //remove not common element in commonsubjects and return true if commonsubjects changed as a result of the call
+                if (commonsubjects.size() > 0) {
+                    group.add(innerStudentId);
+                    visitedStudents.add(innerStudentId);
+                }
+                if (group.size() > 3) {
+                    break;
+                }
+            }
+
+        }
+
+
+        // Display the friend study groups
+        System.out.println();
+        System.out.println("after third matcning, friendtudygruop");
+        int thirdgroupNumber = 1;
+        for (List<Integer> group : friendstudyGroups) {
+            System.out.println("FriendGroup " + thirdgroupNumber + ": ");
+            for (int id : group)
+                System.out.println("studentId " + id + " : " + students.get(id));
+            thirdgroupNumber++;
+        }
+
+        // Display the study groups
+//        int groupNumber = 1;
+        System.out.println("after third matching, new study group");
+        for (List<Integer> group : newstudyGroups) {
+            System.out.println("Group " + thirdgroupNumber + ": ");
+            for (int id : group)
+                System.out.println("studentId " + id + " : " + students.get(id));
+            thirdgroupNumber++;
+        }
+
+        //display not matching student
+        System.out.println();
+        System.out.println("not matching student");
+        for(Student student : studentlist)
+        {
+            int id = student.studentId;
+            if(visitedStudents.contains(id))
+                continue;
+            System.out.println("studentId " +id + " : " + students.get(id));
+        }
+
+    }
+
+    static boolean match_multiplesubject(List<String> currentSubjects, List<String> innerSubjects) {
+
+        if (currentSubjects.containsAll(innerSubjects))
+            return true;
+
+
+        int length = innerSubjects.size();
+        int match = 0;
+
+        for (int j = 0; j < length; j++) {
+            if (currentSubjects.contains(innerSubjects.get(j)))
+                match++;
+        }
+
+        if (match > 1)
+            return true;
+
+        return false;
+    }
+
+    static boolean match_onesubject(List<String> currentSubjects, List<String> innerSubjects) {
+
+
+        int length = innerSubjects.size();
+        int match = 0;
+
+        for (int j = 0; j < length; j++) {
+            if (currentSubjects.contains(innerSubjects.get(j)))
+                match++;
+        }
+
+        if (match > 0)
+            return true;
+
+        return false;
+    }
+
+}
---
+++ src/main/java/sample.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class sample {
+    List<Student> studentList = new ArrayList<>();
+    sample(){
+        generatesamle();
+    }
+    public void generatesamle() {
+        studentList.add(new Student(1, "ECE20014", "ECE20001", "ECE20002", 1));
+        studentList.add(new Student(2, "ECE20001", "ECE20020", "ECE20015", 0));
+        studentList.add(new Student(3, "ECE20018", "ECE20010", "ECE20015", 51));
+        studentList.add(new Student(4, "ECE20019", "ECE20012", "ECE20002", 0));
+
+        studentList.add(new Student(5, "ECE20005", "ECE20007", "ECE20018", 0));
+        studentList.add(new Student(6, "ECE20016", "ECE20011", "ECE20003", 29));
+        studentList.add(new Student(7, "ECE20006", "ECE20020", "ECE20008", 0));
+        studentList.add(new Student(8, "ECE20014", "ECE20016", "ECE20009", 1));
+        studentList.add(new Student(9, "ECE20012", "ECE20004", "ECE20020", 0));
+        studentList.add(new Student(10, "ECE20015", "ECE20002", "ECE20010", 0));
+        studentList.add(new Student(11, "ECE20001", "ECE20011", "ECE20019", 0));
+        studentList.add(new Student(12, "ECE20016", "ECE20003", "ECE20018", 0));
+        studentList.add(new Student(13, "ECE20007", "ECE20013", "ECE20006", 0));
+        studentList.add(new Student(14, "ECE20017", "ECE20020", "ECE20005", 0));
+        studentList.add(new Student(15, "ECE20009", "ECE20008", "ECE20014", 15));
+        studentList.add(new Student(16, "ECE20012", "ECE20002", "ECE20015", 0));
+        studentList.add(new Student(17, "ECE20016", "ECE20011", "ECE20004", 29));
+        studentList.add(new Student(18, "ECE20019", "ECE20016", "ECE20003", 0));
+        studentList.add(new Student(19, "ECE20011", "ECE20016", "ECE20018", 29));
+        studentList.add(new Student(20, "ECE20007", "ECE20011", "ECE20005", 0));
+        studentList.add(new Student(21, "ECE20000", "ECE20001", "ECE20002", 21));
+        studentList.add(new Student(22, "ECE20011", "ECE20002", "ECE20015", 0));
+        studentList.add(new Student(23, "ECE20015", "ECE20010", "ECE20016", 0));
+        studentList.add(new Student(24, "ECE20020", "ECE20002", "ECE20009", 0));
+
+        studentList.add(new Student(25, "ECE20017", "ECE20004", "ECE20019", 0));
+        studentList.add(new Student(26, "ECE20006", "ECE20015", "ECE20012", 57));
+        studentList.add(new Student(27, "ECE20011", "ECE20003", "ECE20014", 0));
+        studentList.add(new Student(28, "ECE20018", "ECE20005", "ECE20008", 0));
+        studentList.add(new Student(29, "ECE20016", "ECE20011", "ECE20009", 29));
+        studentList.add(new Student(30, "ECE20002", "ECE20013", "ECE20020", 0));
+        studentList.add(new Student(31, "ECE20010", "ECE20001", "ECE20017", 0));
+        studentList.add(new Student(32, "ECE20012", "ECE20014", "ECE20003", 1));
+        studentList.add(new Student(33, "ECE20008", "ECE20009", "ECE20006", 0));
+        studentList.add(new Student(34, "ECE20007", "ECE20005", "ECE20016", 0));
+        studentList.add(new Student(35, "ECE20015", "ECE20004", "ECE20018", 0));
+        studentList.add(new Student(36, "ECE20011", "ECE20001", "ECE20000", 21));
+        studentList.add(new Student(37, "ECE20013", "ECE20002", "ECE20019", 0));
+        studentList.add(new Student(38, "ECE20014", "ECE20008", "ECE20010", 0));
+        studentList.add(new Student(39, "ECE20007", "ECE20018", "ECE20016", 0));
+        studentList.add(new Student(40, "ECE20006", "ECE20009", "ECE20012", 0));
+        studentList.add(new Student(41, "ECE20008", "ECE20001", "ECE20012", 15));
+        studentList.add(new Student(42, "ECE20011", "ECE20021", "ECE20010", 0));
+        studentList.add(new Student(43, "ECE20013", "ECE20016", "ECE20012", 0));
+        studentList.add(new Student(44, "ECE20017", "ECE20011", "ECE20002", 0));
+
+        studentList.add(new Student(45, "ECE20011", "ECE20007", "ECE20020", 0));
+        studentList.add(new Student(46, "ECE20018", "ECE20004", "ECE20013", 0));
+        studentList.add(new Student(47, "ECE20005", "ECE20008", "ECE20015", 0));
+        studentList.add(new Student(48, "ECE20012", "ECE20016", "ECE20001", 0));
+        studentList.add(new Student(49, "ECE20009", "ECE20017", "ECE20006", 0));
+        studentList.add(new Student(50, "ECE20002", "ECE20019", "ECE20014", 0));
+        studentList.add(new Student(51, "ECE20007", "ECE20018", "ECE20012", 51));
+        studentList.add(new Student(52, "ECE20003", "ECE20005", "ECE20020", 0));
+        studentList.add(new Student(53, "ECE20014", "ECE20016", "ECE20008", 0));
+        studentList.add(new Student(54, "ECE20006", "ECE20009", "ECE20017", 0));
+        studentList.add(new Student(55, "ECE20015", "ECE20001", "ECE20013", 0));
+        studentList.add(new Student(56, "ECE20020", "ECE20003", "ECE20010", 0));
+        studentList.add(new Student(57, "ECE20012", "ECE20006", "ECE20019", 57));
+        studentList.add(new Student(58, "ECE20008", "ECE20006", "ECE20018", 0));
+        studentList.add(new Student(59, "ECE20017", "ECE20005", "ECE20014", 0));
+        studentList.add(new Student(60, "ECE20004", "ECE20016", "ECE20009", 0));
+
+    }
+}
